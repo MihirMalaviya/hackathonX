811521b08365f414c7485102048fd7d219662084
MihirMalaviya <mihirmalaviya1@gmail.com>
2025-04-26 09:44:41 -0400

    so so far it takes markdown text in python and parses it into html and displays it into a page styled with tailwind css - Mihir

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("Initial commit!") 
+import markdown
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+
+text = """
+
+# this is some simle markdon text
+
+```
+lets test this mannnn
+yay
+ayasudiuahwid
+asdads
+```
+
+## adshihawu
+
+dsaadsads
+""".strip()
+
+
+def md2html(text: str) -> str:
+    """
+    converts text to html
+    :param text: text to convert
+    :return: html c0de
+    """
+
+    html_code = markdown.markdown(
+        text,
+        extensions=[
+            "markdown.extensions.fenced_code",
+            "markdown.extensions.codehilite",
+            "markdown.extensions.tables",
+            "markdown.extensions.footnotes",
+        ],
+    )
+
+    return html_code
+
+
+with open("content.html", "w") as f:
+    tempHTML = md2html(text)
+
+    print(tempHTML)
+    f.write(tempHTML)
+
+
+print()
